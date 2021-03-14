6f1d381144157ac4d05416bc85cbec7ea0d4145b
Aevan Candelaria <aevan.candelaria@gmail.com>
2021-03-14 23:13:04 +0800

    Added Register and Search html in Accounts

--- admincp/page/account/register.php
+++ admincp/page/account/register.php
@@ -1,8 +1,79 @@
     <!-- Begin Page Content -->
     <div class="container-fluid">
-
-        <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800">Register User</h1>
-
+		<form action="" method="">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Name</span>
+                </div>
+                <input type="text" class="form-control" placeholder="Full Name" name="strName" required>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Address</span>
+                </div>
+                <input type="text" class="form-control" placeholder="Metro Manila, Philippines" name="strAddress" required>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Date of Birth</span>
+                </div>
+                <input type="number" class="form-control" placeholder="Year" name="iYear"
+                required  
+                >
+                <input type="number" class="form-control" placeholder="Month" name="iMonth"
+                required
+                >
+                <input type="number" class="form-control" placeholder="Day" name="iDay"
+                required
+                >
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Email</span>
+                </div>
+                <input type="email" class="form-control" placeholder="name@example.com" name="strEmail">
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Password</span>
+                </div>
+                <input type="password" class="form-control" placeholder="Password" name="strPassword">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Confirm Password</span>
+                </div>
+                <input type="password" class="form-control" placeholder="Confirm Password" name="strCPassword">
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Contact No.</span>
+                </div>
+                <input type="text" class="form-control" placeholder="09123456789" name="strContactNo">
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Sex</span>
+                </div>
+                <select class="form-control" name="iSex">
+                    <option value="0">Male</option>
+                    <option value="1">Female</option>
+                </select>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text">Role</span>
+                </div>
+                <select class="form-control" name="iRole">
+                    <option value="-1">BANNED</option>
+                    <option value="0">NORMAL</option>
+                    <option value="1">INVENTORY</option>
+                    <option value="2">FINANCIAL</option>
+                    <option value="3">ADMIN</option>
+                </select>
+            </div>
+            <div class="container text-center">
+                <button type="button" class="btn btn-primary btn-md">Register</button>
+            </div>
+        </form>
     </div>
     <!-- /.container-fluid -->
--- admincp/page/account/search.php
+++ admincp/page/account/search.php
@@ -1,8 +1,112 @@
     <!-- Begin Page Content -->
     <div class="container-fluid">
-
-        <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800">Search User</h1>
 
+        
+
+		<!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+
+            <!-- Topbar Search -->
+            <form
+                class="d-none d-sm-inline-block form-inline mr-auto ml-4 mt-4 navbar-search">
+                <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                        aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button">
+                            <i class="fas fa-search fa-sm"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Birthdate</th>
+                                <th>Email</th>
+                                <th>Contact No.</th>
+                                <th>Sex</th>
+                                <th>Role</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Birthdate</th>
+                                <th>Email</th>
+                                <th>Contact No.</th>
+                                <th>Sex</th>
+                                <th>Role</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <tr>
+                                <td>Tiger Nixon</td>
+                                <td>Edinburgh</td>
+                                <td>2011/04/25</td>
+                                <td>nixontiger@ebanmerch.com</td>
+                                <td>091234567897</td>
+                                <td>Male</td>
+                                <td>System Architect</td>
+                            </tr>
+                            <tr>
+                                <td>Garrett Winters</td>
+                                <td>Tokyo</td>
+                                <td>2011/07/25</td>
+                                <td>wintersgarret@ebanmerch.com</td>
+                                <td>091234567897</td>
+                                <td>Male</td>
+                                <td>Accountant</td>
+                            </tr>
+                            <tr>
+                                <td>Ashton Cox</td>
+                                <td>San Francisco</td>
+                                <td>2009/01/12</td>
+                                <td>coxashton@ebanmerch.com</td>
+                                <td>091234567897</td>
+                                <td>Male</td>
+                                <td>Junior Technical Author</td>
+                            </tr>
+                            <tr>
+                                <td>Cedric Kelly</td>
+                                <td>Edinburgh</td>
+                                <td>2012/03/29</td>
+                                <td>kellycedric@ebanmerch.com</td>
+                                <td>091234567897</td>
+                                <td>Male</td>
+                                <td>Senior Javascript Developer</td>
+                            </tr>
+                            <tr>
+                                <td>Airi Satou</td>
+                                <td>Tokyo</td>
+                                <td>2008/11/28</td>
+                                <td>satouairi@ebanmerch.com</td>
+                                <td>091234567897</td>
+                                <td>Female</td>
+                                <td>Accountant</td>
+                            </tr>
+                            <tr>
+                                <td>Brielle Williamson</td>
+                                <td>New York</td>
+                                <td>2012/12/02</td>
+                                <td>williamsonbrielle@ebanmerch.com</td>
+                                <td>091234567897</td>
+                                <td>Female</td>
+                                <td>Integration Specialist</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
     <!-- /.container-fluid -->
