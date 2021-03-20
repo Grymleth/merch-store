6e2f4e6a8de7a8e9d4048625d345feb1a10abd1c
Aevan Candelaria <aevan.candelaria@gmail.com>
2021-03-21 00:25:20 +0800

    Added Transaction Log HTML

--- admincp/page/finance/transaction.php
+++ admincp/page/finance/transaction.php
@@ -1,8 +1,84 @@
-    <!-- Begin Page Content -->
-    <div class="container-fluid">
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Transaction Logs</h1>
 
-        <!-- Page Heading -->
-        <h1 class="h3 mb-4 text-gray-800">Transaction Logs</h1>
-
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Transaction List</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Transaction ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Product Name</th>
+                            <th>Product Category</th>
+                            <th>Transaction Date</th>
+                            <th>Quantity</th>
+                            <th>Total Price</th>
+                            <th>Delivery Status</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>Transaction ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Product Name</th>
+                            <th>Product Category</th>
+                            <th>Transaction Date</th>
+                            <th>Quantity</th>
+                            <th>Total Price</th>
+                            <th>Delivery Status</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Aevan Candelaria</td>
+                            <td>aevan.candelaria@gmail.com</td>
+                            <td>Creeper Hoodie</td>
+                            <td>Hoodies</td>
+                            <td>June 7, 1999</td>
+                            <td>69</td>
+                            <td>10,000</td>
+                            <td>Processing</td>
+                            <td><button class="btn btn-primary">Edit</button></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Dan Satparam</td>
+                            <td>dmsatparam@gmail.com</td>
+                            <td>Cat in Bread T-Shirt</td>
+                            <td>Shirt</td>
+                            <td>March 20, 2021</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>Packed</td>
+                            <td><button class="btn btn-primary">Edit</button></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>John Kenneth Andales</td>
+                            <td>jka@rai2.com</td>
+                            <td>T-shirt</td>
+                            <td>Shirt</td>
+                            <td>March 22, 2021</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>Shipped</td>
+                            <td><button class="btn btn-primary">Edit</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-    <!-- /.container-fluid -->
+</div>
+<!-- /.container-fluid -->
