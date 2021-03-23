104f56ec49ee74dc92edc2bb07498298207cbf56
Raitou <raitoukun@hotmail.com>
2021-03-23 20:19:19 +0800

    removed smt

--- admincp/classes/class.transaction.php
+++ admincp/classes/class.transaction.php
@@ -6,26 +6,6 @@ class Transaction{
         $this->transactionDB = new Database("127.0.0.1", "root", "", "transactions");
 	}
 
-    public function viewTransaction($strEmail, $strPass){
-        if(!empty($ret = Common::validateEmail($strEmail))) return $ret;
-
-        $hash_pass =  hash("sha512", $strEmail.$strPass);
-        $result = $this->accountDB->query_fetch_single("SELECT AccountID FROM users WHERE Email = ? AND Pass = ?", array($strEmail, $hash_pass));
-        if(!is_array($result)) return "Login Failed";
-
-        $accountInfo = $this->getAccountInfo(intval($result["AccountID"]));
-        if(!is_array($accountInfo)) return "System Error! Contact Administrator";
-        if(Common::getRoleName(intval($accountInfo["RoleID"])) == "BANNED_USER" || Common::getRoleName(intval($accountInfo["RoleID"])) == "NORMAL_USER")
-            return "Login Failed";
-
-        $session = Session::getInstance();
-        $session->accountID = $accountInfo["AccountID"];
-        $session->roleID = $accountInfo["RoleID"];
-
-        header("Location: dashboard/home");
-        die();
-    }
-
     public function changeDeliveryStatus($transactionID, $newDeliveryStatus){
         $result = $this->transactionDB->query("UPDATE deliverystatus SET DeliveryCode = ? WHERE TransactionID = ?", array($newDeliveryStatus, $transactionID));
         if(!$result) return "Delivery status request not successfully changed for unknown reasons. Contact Administrator";
